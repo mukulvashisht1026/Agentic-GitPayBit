9035a30db903694fa3f4d5a58cccc3e11e9c53f9
Mukul Vashisht <mukulvashisht1026@gmail.com>
2025-03-15 15:33:00 +0530

    updateded port for heroku

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
